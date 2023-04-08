e502f3d50e049a0c1e144d9c0b796ff4d6cb4da5
splean101 <101splean@gmail.com>
2023-04-08 13:40:54 +0300

    4

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { createRequire } from 'node:module';
-import fs from 'fs';
+import fs, { promises } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,27 +12,31 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  fs.readFile(path.join(__dirname, './package.json'), (err, content) => {
-    if (err) {
-      console.error(err);
-    }
-    res.send(`<p>${content.toString()}</p>`);
-  });
+// app.get('/', (req, res) => {
+//   fs.readFile(path.join(__dirname, './package.json'), (err, content) => {
+//     if (err) {
+//       console.error(err);
+//     }
+//     res.send(`<p>${content.toString()}</p>`);
+//   });
+// });
 
-  //res.send('<h1>Welcome</h1>');
+app.get('/', async (req, res) => {
+  try {
+    const content = await promises.readFile(
+      path.join(__dirname, './package.json')
+    );
+    res.send(`<p>${content.toString()}</p>`);
+  } catch (err) {
+    console.error(err);
+  }
 });
+
 if (process.env.NODE_ENV == 'development') {
   console.log('development mode');
 } else {
   console.log('production mode');
 }
-// fs.readFile(path.join(__dirname, './package.json'), (err, content) => {
-//   if (err) {
-//     console.error(err);
-//   }
-//   res.send(`<p>${content.toString()}</p>`);
-// });
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
